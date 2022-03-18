d0e6f698c2de33adc3306550fd2a82a9fec4a0b5
EEEEnusya <yafilatovaa@yandex.ru>
2022-03-18 19:05:10 +0300

    ComMiTT1

---
+++ lab2/hello_world.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+#include <string>//добавление библиотекм
+int main () {
+std::string name;
+std:: cin>> name;
+std::cout <<"Hello, world from "<<name <<  std::endl;
+return  0;
+} 
