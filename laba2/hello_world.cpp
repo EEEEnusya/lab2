e420077be428ac3a6294f41e850768be5322a518
EEEEnusya <yafilatovaa@yandex.ru>
2022-03-18 14:03:35 +0300

    ComMiTT

--- laba2/hello_world.cpp
+++ laba2/hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main () {
-std::string name;
-std:: cin>> name;
-std::cout <<"Hello, world from "<<name <<  std::endl;
-return  0;
-} 
+int
+main()
+{
+  std::string name;
+  std::cin >> name;
+  std::cout << "Hello, world from " << name << std::endl;
+  return 0;
+}
