267a7dec81c8d24754c9116406ae139a45d84fac
EEEEnusya <yafilatovaa@yandex.ru>
2022-03-18 11:16:31 +0300

    third commit

--- laba2/hello_world.cpp
+++ laba2/hello_world.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main () {
-std::cout <<"Hello, world!"<< std::endl;
+std::string name;
+std:: cin>> name;
+std::cout <<"Hello, world from "<<name <<  std::endl;
 return  0;
 } 
